c0d4bf71116c7e14b9ae3197fdc26a4f7f21e666
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-23 21:44:59 +0530

    init: complete code

---
+++ script.js
@@ -0,0 +1,46 @@
+// STUDENT
+// let studentName;
+// let emailAddress;
+// let mobileNumber;
+// let isWhatsappChecked;
+// let password;
+// let confirmPassword;
+// let isTermsChecked;
+
+// COMPANY
+let userName;
+let companyName;
+let companyDesignation;
+let mobileNumber;
+let isWhatsappChecked;
+let workEmail;
+let isWorkEmailChecked;
+let password;
+let confirmPassword;
+let isTermsChecked;
+
+//  program to swap the values of two variables
+let a=5;
+let b=10;
+let temp;
+temp=a;
+a=b;
+b=temp;
+console.log(a,b)
+
+// program that converts a temperature from Celsius to Fahrenheit
+let celsiusTemperature = 30; // Replace with the desired Celsius temperature
+let fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+console.log(fahrenheitTemperature);
+
+// program that calculates the area of a circle
+let circleRadius = 5;
+const PI = 3.14;
+let circleArea;
+circleArea = PI * (circleRadius**2);
+console.log(circleArea);
+
+// program that calculates the factorial of a given number
+let number = 4;
+let factorial = number * (number - 1) * (number - 2) * (number - 3);
+console.log(factorial);
